7d75e74debd960bb7cd0041d2258258b9ed29eeb
luciasalvatierra <lucia.salvatierra@opendeusto.es>
2026-04-07 14:06:10 +0200

    menus

--- src/menus.h
+++ src/menus.h
@@ -2,8 +2,24 @@
 #define MENUS_H
 
 void menuPrincipal();
+void menuGestionColegiales();
+void menuGestionHabitaciones();
+void menuGestionIncidencias();
+void menuGestionPagos();
+void menuGestionReservasActividades();
+void menuLlegadasNocturnas();
+void menuSalidasVacaciones();
+void menuNormasSanciones();
 void menuPropuestasYComentarios();
 void mostrarMenuPrincipal();
+void mostrarMenuGestionColegiales();
+void mostrarMenuGestionHabitaciones();
+void mostrarMenuGestionIncidencias();
+void mostrarMenuGestionPagos();
+void mostrarMenuGestionReservasActividades();
+void mostrarMenuLlegadasNocturnas();
+void mostrarMenuSalidasVacaciones();
+void mostrarMenuNormasSanciones();
 void mostrarMenuPropuestasYComentarios();
 
 #endif
