a03c3aa554d60735cbdf690d568c370080e8f3ab
luciasalvatierra <lucia.salvatierra@opendeusto.es>
2026-04-07 13:00:19 +0200

    menus

---
+++ src/menus.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "menus.h"
+
+void mostrarMenuPrincipal() {
+    printf("\nMENÚ PRINCIPAL\n");
+    printf("1. Gestión de colegiales\n");
+    printf("2. Gestión de habitaciones\n");
+    printf("3. Gestión de incidencias\n");
+    printf("4. Gestión de pagos\n");
+    printf("5. Reservas y actividades\n");
+    printf("6. Llegadas nocturnas\n");
+    printf("7. Salidas por vacaciones\n");
+    printf("8. Normas y sanciones\n");
+    printf("9. Propuestas y comentarios\n");
+    printf("10. Cambiar mi contraseña\n");
+    printf("11. Cerrar sesión\n");
+}
+
+void mostrarMenuPropuestasYComentarios() {
+    printf("\nPROPUESTAS Y COMENTARIOS\n");
+    printf("1. Registrar propuesta/comentario\n");
+    printf("2. Ver pendientes\n");
+    printf("3. Buscar por colegial\n");
+    printf("4. Buscar por categoría\n");
+    printf("5. Cambiar estado\n");
+    printf("6. Cerrar propuesta\n");
+    printf("7. Volver\n");
+}
+
+
